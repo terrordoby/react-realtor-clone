1068338272e6d581039f796363f0adf7c46bcd20
Gabriel Neto <gabriel.jorge@softwell.com.br>
2023-02-22 11:50:35 -0300

    Create header Section

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Sigin from "./pages/Sigin";
+import SignUp from "./pages/SignUp";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Sigin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+
+const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function pathMathRoute(route: string) {
+    if(route === location.pathname) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between max-w-6xl mx-auto items-center">
+        <div>
+          <img
+            className="h-5 cursor-pointer"
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="Logo"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-6">
+            <li
+              onClick={() => navigate("/")}
+              className={`py-3 text-sm font-semibold text-gray-400
+              border-b-[3px] border-b-transparent cursor-pointer ${pathMathRoute("/")
+              && "text-black border-b-red-500"}`}>Home</li>
+            <li
+              onClick={() => navigate("/offers")}
+              className={`py-3 text-sm font-semibold text-gray-400
+              border-b-[3px] border-b-transparent cursor-pointer ${pathMathRoute("/offers")
+              && "text-black border-b-red-500"}`}>Offers</li>
+            <li
+              onClick={() => navigate("/sigin-in")}
+              className={`py-3 text-sm font-semibold text-gray-400
+              border-b-[3px] border-b-transparent cursor-pointer ${pathMathRoute("/sigin-in")
+              && "text-black border-b-red-500"}`}>Sigin in</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
