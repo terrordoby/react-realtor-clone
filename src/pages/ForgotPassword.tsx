f342ca9e4f96732710cfaf80515a74eb9c0bc010
Gabriel Neto <gabriel.jorge@softwell.com.br>
2023-02-25 14:28:48 -0300

    Complete the forgot password

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,22 +1,35 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useState } from "react";
-import {AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { getAuth } from "firebase/auth";
+import React, { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import {sendPasswordResetEmail} from "firebase/auth";
 import OAuth from "../components/OAuth";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState<string>("");
 
+  async function handleSubmitForgotPassword(e: FormEvent) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was send");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
+  }
+
   return (
     <section>
-      <h1 className="text-3xl text-center mt-5 font-bold">Sign In</h1>
+      <h1 className="text-3xl text-center mt-5 font-bold">Forgot Password</h1>
       <div className="flex max-w-6xl mx-auto flex-wrap justify-center
       items-center px-6 py-12">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img className="w-full rounded-2xl" src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80" alt="" />
         </div>
         <div className="md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form action="">
+          <form onSubmit={handleSubmitForgotPassword} action="">
             <div className="mb-6">
               <input
                 className="w-full px-4 py-2 text-xl rounded text-gray-700 bg-white
@@ -43,7 +56,7 @@ const ForgotPassword = () => {
               hover:bg-blue-700 transition duration-200 shadow-sm font-medium
                active:bg-blue-800"
               type="submit">
-                Send Resed Password
+                Send Reset Password
             </button>
             <div
               className="flex items-center justify-center my-4 before:border-t
