d756feb2b9e407941440029928decae2d1d71d65
Gabriel Neto <gabriel.jorge@softwell.com.br>
2023-02-28 16:57:48 -0300

    Create Private Route and custom hook

---
+++ src/hooks/useAuthStatus.tsx
@@ -0,0 +1,21 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [checkingStatus, setCheckingStatus] = useState<boolean>(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  },[]);
+
+  return {loggedIn, checkingStatus};
+};
+
+export default useAuthStatus;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,60 @@
-import React from "react";
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Profile = () => {
+  const auth =  getAuth();
+  const [name, setName] = useState<string | undefined >(auth.currentUser?.displayName as string | undefined);
+  const [email, setEmail] = useState<string | undefined>(auth.currentUser?.email as string | undefined);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+
   return (
-    <div>Profile</div>
+    <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+      <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+      <div className="w-full md:w-[50%] mt-6 px-3">
+        <form>
+          {/* input Nome */}
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled
+            className="w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded
+            transition ease-in-out"
+          />
+          <input
+            type="text"
+            id="name"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled
+            className="w-full mb-6 px-4 py-2 text-xl text-gray-700  bg-white border border-gray-300 rounded
+            transition ease-in-out"
+          />
+          <div
+            className="flex justify-between mb-6 whitespace-nowrap text-sm sm:text-lg">
+            <p className="flex items-center">Do you want to change your name ?
+              <span
+                className="text-red-600 hover:text-red-700 transition ease-in-out duration-150
+                ml-1 cursor-pointer">
+                  Edit
+              </span>
+            </p>
+            <p
+              className="text-blue-600 hover:text-blue-800 cursor-pointer transition ease-in-out duration-150"
+              onClick={handleLogout}>
+                Sign Out
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
